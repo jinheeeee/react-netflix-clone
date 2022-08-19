ec9bb02698708bc368986eb2c3a3111da4d144e1
Stella <wlsl5353@nate.com>
2022-08-19 11:49:10 +0900

    env add

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "b7ee21432e1ba576a4249533b990b3e5",
+    api_key: process.env.REACT_APP_KEY,
     language: "ko-KR",
   },
 });
